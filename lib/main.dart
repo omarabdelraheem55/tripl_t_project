7e72a5db4ac034e2e8076189adb372794120a3fb
omarabdelraheem55 <zazazaomarsaleh55@gmail.com>
2022-09-24 15:20:03 -0700

    _ main

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
                     ),
                   ),
                 ),
-                nextScreen: SignIn(),),),);
+                nextScreen: Screen2(),),),);
 
   }
 }
